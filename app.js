1701e6b87b48f5b528ed7f897e0f888e5dcd3e1b
DamianOdair <pdog131092@gmail.com>
2021-03-11 17:26:49 -0600

    cambios a borrar

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ window.onload = function () {
     document.onmousemove = function(ev) {
         var x = ev.clientX / 10;
         var y = ev.clientY / 10;
-        document.body.style.backgroundPosition = x + "px " + y + "px"
+        /*document.body.style.backgroundPosition = x + "px " + y + "px"*/
+        document.getElementById('header__castillo--luna').style.left=`${x}px`;
+        document.getElementById('header__castillo--luna').style.top=`${y}px`;
+        document.getElementById('header__castillo--luna').style=`position:absolute; left:${x}px; top:${y}px;`;
     }
 }
 
